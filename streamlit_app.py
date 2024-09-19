da1263ed3bdbdf53be265b797656b78205f652f3
carlkuo-nxp <carl.kuo@nxp.com>
2024-09-19 14:35:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,8 @@ with st.sidebar:
   Leadframesupplier = st.selectbox('Leadframe supplier; MHT=1 / HDS=0',('0','1'))
   Rough = st.selectbox('Rough design? ; Yes=1 / No=0' ,('0','1'))
   Leaddesign = st.selectbox('Lead design; long-half etching=0 / long-inner lead=1 / normal=2',('0','1','2'))
+  Square = st.selectbox('IC is a square? not a square=0 / a square=1',('0','1'))
+  Area = st.slider('Area',2.295, 100, 10)
   AssemblyCG = st.slider('Assembly CG', 0, 55, 25 )
   Outline = st.slider('Outline', 0, 55, 25 )
   Subpackage = st.slider('Subpackage', 0, 93, 25)
