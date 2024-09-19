6b916b35c4a0001eef66c66c045d845acd60d38d
carlkuo-nxp <carl.kuo@nxp.com>
2024-09-19 14:19:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ with st.expander('Data visualization'):
 ##Data preparation##
 with st.sidebar:
   st.header('Input features')
-  Leadframesupplier = st.selectbox('Leadframe supplier',('0','1'))
+  Leadframesupplier = st.selectbox('Leadframe supplier; MHT=1 / HDS=0',('0','1'))
   AssemblyCG = st.slider('Assembly CG', 0, 55, 25 )
   Outline = st.slider('Outline', 0, 55, 25 )
   Subpackage = st.slider('Subpackage', 0, 93, 25)
