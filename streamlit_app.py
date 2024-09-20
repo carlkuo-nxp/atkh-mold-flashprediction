57ab662f362da784c97aa727f88f795608ba4791
carlkuo-nxp <carl.kuo@nxp.com>
2024-09-20 13:33:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write('This is a machine learning app and builds a machine learning model!')
 
 with st.expander('Data'):
   st.write('**Raw data**')
-  df = pd.read_csv('label_encoding_std-Original_V01.csv')
+  df = pd.read_csv('label_encoding_std-Original_V02.csv')
   df
 
   st.write('**Input Variable**')
