9d15adc3053b13f2a5d1ee9633c0363c07ad9926
carlkuo-nxp <carl.kuo@nxp.com>
2024-09-20 09:26:34 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,36 +25,36 @@ with st.expander('Data visualization'):
 ##Data preparation##
 with st.sidebar:
   st.header('Input features')
-  Leadframesupplier = st.selectbox('Leadframe supplier; MHT=1 / HDS=0',('0','1'))
+  Leadframe supplier = st.selectbox('Leadframe supplier; MHT=1 / HDS=0',('0','1'))
   Rough = st.selectbox('Rough design? ; Yes=1 / No=0' ,('0','1'))
-  Leaddesign = st.selectbox('Lead design; long-half etching=0 / long-inner lead=1 / normal=2',('0','1','2'))
+  Lead design = st.selectbox('Lead design; long-half etching=0 / long-inner lead=1 / normal=2',('0','1','2'))
   Square = st.selectbox('IC is a square? no=0 / yes=1',('0','1'))
   Dimple = st.selectbox('Dimple design? ; Deep=0 / No=1 / Normal=2',('0','1','2'))
-  Wiretype = st.selectbox('Wire type ; Au=0 / Cu=1',('0','1'))
+  Wire type = st.selectbox('Wire type ; Au=0 / Cu=1',('0','1'))
   Compound = st.selectbox('Compound',('0','1','2','3','4','5'))
   Taping = st.selectbox('Taping; post-taping=0 / pre-taping=1',('0','1'))
   Area = st.slider('Area', 2.295, 100.000, 10.000 )
-  Unitperstrip = st.slider('Unit per strip', 110, 3990, 500)
-  AssemblyCG = st.slider('Assembly CG', 0, 55, 25 )
+  Unit per strip = st.slider('Unit per strip', 110, 3990, 500)
+  Assembly CG = st.slider('Assembly CG', 0, 55, 25 )
   Outline = st.slider('Outline', 0, 55, 25 )
   Subpackage = st.slider('Subpackage', 0, 93, 25)
-  Leadframe12NC = st.slider('Leadframe 12NC', 0, 85, 25)
+  Leadframe 12NC = st.slider('Leadframe 12NC', 0, 85, 25)
 
 ##Create a dataframe for the input features##
-data = { 'Leadframesupplier' : Leadframesupplier,
+data = { 'Leadframe supplier' : Leadframe supplier,
         'Rough' : Rough,
-        'Lead design' : Leaddesign,
+        'Lead design' : Lead design,
         'Square' : Square,
         'Dimple' : Dimple,
-        'Wire type' : Wiretype,
+        'Wire type' : Wire type,
         'Compound' : Compound,
         'Taping' : Taping,
         'Area' : Area,
-        'Unit per strip' : Unitperstrip,
-        'Assembly CG' : AssemblyCG,
+        'Unit per strip' : Unit per strip,
+        'Assembly CG' : Assembly CG,
         'Outline' : Outline,
         'Subpackage' : Subpackage,
-        'Leadframe 12NC' :  Leadframe12NC}
+        'Leadframe 12NC' :  Leadframe 12NC}
 input_df = pd.DataFrame(data, index=[0])
 input_label_encoding = pd.concat([input_df, X], axis=0) ##axis=0 append by row, o.w. axis=1 append by column##
 
