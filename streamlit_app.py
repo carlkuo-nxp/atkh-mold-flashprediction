74e1201511fa8e5d19538a10f4020d4c04e00ea5
carlkuo-nxp <carl.kuo@nxp.com>
2024-09-20 10:23:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,31 +30,31 @@ with st.sidebar:
   Lead design = st.selectbox('Lead design; long-half etching=0 / long-inner lead=1 / normal=2',('0','1','2'))
   Square = st.selectbox('IC is a square? no=0 / yes=1',('0','1'))
   Dimple = st.selectbox('Dimple design? ; Deep=0 / No=1 / Normal=2',('0','1','2'))
-  Wire type = st.selectbox('Wire type ; Au=0 / Cu=1',('0','1'))
+  Wiretype = st.selectbox('Wire type ; Au=0 / Cu=1',('0','1'))
   Compound = st.selectbox('Compound',('0','1','2','3','4','5'))
   Taping = st.selectbox('Taping; post-taping=0 / pre-taping=1',('0','1'))
   Area = st.slider('Area', 2.295, 100.000, 10.000 )
-  Unit per strip = st.slider('Unit per strip', 110, 3990, 500)
+  Unitperstrip = st.slider('Unit per strip', 110, 3990, 500)
   Assembly CG = st.slider('Assembly CG', 0, 55, 25 )
   Outline = st.slider('Outline', 0, 55, 25 )
   Subpackage = st.slider('Subpackage', 0, 93, 25)
-  Leadframe 12NC = st.slider('Leadframe 12NC', 0, 85, 25)
+  Leadframe12NC = st.slider('Leadframe 12NC', 0, 85, 25)
 
 ##Create a dataframe for the input features##
-data = { 'Leadframe supplier' : Leadframe supplier,
+data = { 'Leadframesupplier' : Leadframe supplier,
         'Rough' : Rough,
-        'Lead design' : Lead design,
+        'Leaddesign' : Leaddesign,
         'Square' : Square,
         'Dimple' : Dimple,
-        'Wire type' : Wire type,
+        'Wiretype' : Wiretype,
         'Compound' : Compound,
         'Taping' : Taping,
         'Area' : Area,
-        'Unit per strip' : Unit per strip,
-        'Assembly CG' : Assembly CG,
+        'Unitperstrip' : Unitperstrip,
+        'AssemblyCG' : AssemblyCG,
         'Outline' : Outline,
         'Subpackage' : Subpackage,
-        'Leadframe 12NC' :  Leadframe 12NC}
+        'Leadframe12NC' :  Leadframe12NC}
 input_df = pd.DataFrame(data, index=[0])
 input_label_encoding = pd.concat([input_df, X], axis=0) ##axis=0 append by row, o.w. axis=1 append by column##
 
